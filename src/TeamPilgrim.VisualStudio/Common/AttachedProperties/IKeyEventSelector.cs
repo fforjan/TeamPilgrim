b541e4fb6056d3dfedd77e0201d8ff4252abd29f
BruceMellows <github@mellows.tv>
2013-09-07 08:08:22 +1000

    Discriminate Return / Return+Shift
    Now easier to add new key down preview mappings, but didn't bother to go
    back and modify other ones

---
+++ src/TeamPilgrim.VisualStudio/Common/AttachedProperties/IKeyEventSelector.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace JustAProgrammer.TeamPilgrim.VisualStudio.Common.AttachedProperties
+{
+	public interface IKeyEventSelector
+	{
+		bool CanExecute(object sender, KeyEventArgs e);
+	}
+}
---
+++ src/TeamPilgrim.VisualStudio/Common/AttachedProperties/PreviewKeyDownReturnOnly.cs
@@ -0,0 +1,15 @@
+﻿namespace JustAProgrammer.TeamPilgrim.VisualStudio.Common.AttachedProperties
+{
+	using System.Windows.Input;
+
+	public class PreviewKeyDownReturnOnly : PreviewKeyDown<PreviewKeyDownReturnOnly.ReturnOnlySelector>
+	{
+		public class ReturnOnlySelector : IKeyEventSelector
+		{
+			public bool CanExecute(object sender, KeyEventArgs e)
+			{
+				return e.Key == Key.Return && System.Windows.Forms.Control.ModifierKeys == System.Windows.Forms.Keys.None;
+			}
+		}
+	}
+}
